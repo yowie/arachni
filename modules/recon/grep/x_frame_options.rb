749bdbd621d539c178b32895d547ce7fdb7839fb
yowie <yowie@lab.yowie.me>
2013-12-15 12:20:38 +0300

    x_frame stub

--- modules/recon/grep/x_frame_options.rb
+++ modules/recon/grep/x_frame_options.rb
@@ -1,20 +1,19 @@
 =begin
-Copyright 2010-2013 Tasos Laskos <tasos.laskos@gmail.com>
+    Copyright 2010-2013 Tasos Laskos <tasos.laskos@gmail.com>
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 =end
 
-#
 # Check for lack of X-FRAME-OPTIONS header.
 #
 # @author Iain Funnell/yowie @iainfunnell
@@ -24,33 +23,40 @@ limitations under the License.
 class Arachni::Modules::XFrameOptions < Arachni::Module::Base
 
     def run
-        page.headers.each do |header|
+      spider.on_each_page do |page|
+        print_status "test: #{page.url}"
+      end  
+      #page.headers.each do |header|
+        #  binding.pry
+
            # next if cookie.http_only? || audited?( cookie.name )
 
            # log( var: cookie.name, element: cookie.type, )
           #  audited( cookie.name )
-        end
+        #end
     end
+
 
     def self.info
         {
-            name: 'X-FRAME-OPTIONS check',
-            description: %q{Checks if the X-FRAME-OPTIONS header is configured to prevent framing attacks.},
-            elements: [ Element::HEADER ],
-            author: 'Iain Funnell @iainfunnell',
-            version: '0.1.1',
-            targets: %w(Generic),
-            references: {
-                'ClickJacking - OWASP' => 'https://www.owasp.org/index.php/Clickjacking'
-            ,}
-            issue: {
-                name: %q{X-FRAME-OPTIONS header missing},
-                description: %q{The site has not enabled the X-FRAME-OPTIONS header 
-                which makes it succeptible to ClickJacking/Framing attacks.},
-                cwe: '200',
-                severity: Severity::INFORMATIONAL,
-                remedy_guidance: %q{Set the 'X-FRAME-OPTIONS' header in server responses.},
-            }
+            name:        'X-FRAME-OPTIONS headers',
+            description: %q{Logs pages that do not set the X-FRAME-OPTIONS headers},
+            elements:    [ Element::HEADER ],
+            author:      'Iain "yowie" Funnell @iainfunnell',
+            version:     '0.1.1',
+            targets:     %w(Generic),
+            references:  {
+                'ClickJacking - OWASP' => 'https://www.owasp.org/index.php/ClickJacking'
+            },
+            issue:       {
+                name:            %q{HttpOnly cookie},
+                description:     %q{The logged cookie does not have the HttpOnly
+    flag set which makes it succeptible to maniplation via client-side code.},
+                cwe:             '200',
+                severity:        Severity::INFORMATIONAL,
+                remedy_guidance: %q{blerg},
+            },
+            max_issues: 1
         }
     end
 
